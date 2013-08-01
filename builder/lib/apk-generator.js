e5d794f4816b3edee1637d1dde8ca25c62a1a593
James Hugman <jhugman@jhugman-10147.local>
2013-08-01 23:08:11 +0100

    Move functionality from cli.js into new generator object, which includes a disk cache, and can be included in a server context.

---
+++ builder/lib/apk-generator.js
@@ -0,0 +1,122 @@
+var _ = require("underscore"),
+    url = require("url"),
+    fs = require("fs.extra"),
+    path = require("path"),
+
+    apk = require("./apk-project-builder");
+
+function createDir(defaultDir, dir) {
+  dir = dir || defaultDir;
+  dir = path.resolve(this.cacheDir, dir);
+  fs.mkdirRecursiveSync(dir);
+  return dir;
+}
+
+
+function ApkGenerator (buildDir, cacheDir) {
+  this.buildDir = buildDir || process.env.TMPDIR;
+  if (cacheDir) {
+    this.cacheDir = path.resolve(process.cwd(), cacheDir);
+  } else {
+    this.cacheDir = path.resolve(__dirname, "..", "cache");
+  }
+
+}
+
+_.extend(ApkGenerator.prototype, {
+
+  generate: function (manifestUrl, overideManifestPath, cb) {
+
+    if (arguments.length === 2 && _.isFunction(overideManifestPath)) {
+      cb = overideManifestPath;
+      overideManifestPath = undefined;
+    }
+
+    var self = this,
+        androidifier = require("./manifest-androidifier"),
+        packageName = androidifier.packageName(manifestUrl),
+        loader, dirname, manifestFilename, projectBuilder,
+        appBuildDir = path.join(this.buildDir, packageName);
+
+    if (/^\w+:\/\//.test(manifestUrl)) {
+      dirname = url.resolve(manifestUrl, ".");
+      manifestFilename = _(url.parse(manifestUrl).pathname.split("/")).last();
+    } else {
+      dirname = path.dirname(path.resolve(process.cwd(), manifestUrl));
+      manifestFilename = path.basename(manifestUrl);
+    }
+    manifestUrl = overideManifestPath || manifestUrl;
+
+    var cacheDir, cachedFile;
+    if (self.cacheDir) {
+      cacheDir = self.createDir("apks/" + packageName);
+      cachedFile = path.join(cacheDir, "application.apk");
+
+
+      if (fs.existsSync(cachedFile)) {
+        if (cb) {
+          cb(null, cachedFile);
+        }
+        return;
+      }
+    }
+
+
+
+
+    loader = require("./file-loader").create(dirname);
+
+    projectBuilder = new apk.ApkProjectCreator("template", appBuildDir, loader);
+
+
+    loader.load(manifestFilename, function (err, string) {
+      if (err) {
+        console.error("Cannot load manifest: " + err);
+        return;
+      }
+      try {
+        var manifest = JSON.parse(string);
+        projectBuilder.create(manifestUrl, manifest, function () {
+          projectBuilder.build(self.createDir("keys"), function (err, apkLoc) {
+
+            if (!err) {
+              if (self.cacheDir) {
+
+                if (fs.existsSync(cachedFile)) {
+                  fs.unlinkSync(cachedFile);
+                }
+                fs.linkSync(apkLoc, cachedFile);
+                apkLoc = cachedFile;
+              }
+            }
+            if (cb) {
+              cb(err, apkLoc);
+            }
+            if (!err) {
+              projectBuilder.cleanup();
+            }
+          });
+        });
+      } catch (e) {
+        if (e.stack) {
+          console.error(e.stack);
+        }
+      }
+    });
+
+
+  },
+
+  createDir: function (defaultDir, dir) {
+    dir = dir || defaultDir;
+    dir = path.resolve(this.cacheDir, dir);
+    fs.mkdirRecursiveSync(dir);
+    return dir;
+  }
+
+});
+
+
+module.exports = {
+  ApkGenerator: ApkGenerator
+};
--- builder/lib/cli.js
+++ builder/lib/cli.js
@@ -1,14 +1,8 @@
 #!/usr/bin/env node
 "use strict";
 var optimist = require("optimist"),
-    fs = require("fs.extra"),
-    _ = require("underscore"),
     path = require("path"),
-    url = require("url"),
-    apk = require("./apk-project-builder");
-
-    fs.existsSync = fs.existsSync || require("path").existsSync;
-
+    fs = require("fs");
 
 var argv = optimist
     .usage('Usage: $0 {OPTIONS}')
@@ -24,6 +18,10 @@ var argv = optimist
         alias: "d",
         desc: "Use this directory as the temporary project directory",
         default: path.resolve(process.env.TMPDIR, "app")
+    })
+    .option("output", {
+        alias: "o",
+        desc: "The output APK filename"
     })
     .option('help', {
         alias: "?",
@@ -42,55 +40,23 @@ var argv = optimist
     })
     .argv;
 
-
-var loader, dirname, manifestFilename, manifestUrl = argv.manifest;
-if (/^\w+:\/\//.test(manifestUrl)) {
-  dirname = url.resolve(manifestUrl, ".");
-  manifestFilename = _(url.parse(manifestUrl).pathname.split("/")).last();
-} else {
-  dirname = path.dirname(path.resolve(process.cwd(), manifestUrl));
-  manifestFilename = path.basename(manifestUrl);
-}
-manifestUrl = argv.overideManifest || manifestUrl;
-var loader = require("./file-loader").create(dirname);
-var projectBuilder = new apk.ApkProjectCreator("template", argv.tmpDir, loader);
-
-function createDir(defaultDir, dir) {
-  dir = dir || defaultDir;
-  dir = path.resolve(__dirname, "..", dir);
-  fs.mkdirRecursiveSync(dir);
-  return dir;
-}
+var ApkGenerator = require("./apk-generator").ApkGenerator,
+    generator = new ApkGenerator(argv.tmpDir);
 
 
-loader.load(manifestFilename, function (err, string) {
-  if (err) {
-    console.error("Cannot load manifest: " + err);
-    return;
-  }
-  try {
-    var manifest = JSON.parse(string);
-    projectBuilder.create(manifestUrl, manifest, function () {
-      projectBuilder.build(createDir("cache/keys"), function (err, apkLoc) {
+generator.generate(argv.manifest, argv.overideManifest, function (err, apkLoc) {
+  var output;
+  if (!err) {
 
-        if (!err) {
-          var androidifier = require("./manifest-androidifier"),
-              packageName = androidifier.packageName(manifestUrl),
-              cacheDir = createDir("cache/apks/" + packageName),
-              cachedFile = path.join(cacheDir, "application.apk");
-          if (fs.existsSync(cachedFile)) {
-            fs.unlinkSync(cachedFile);
-          }
-          fs.linkSync(apkLoc, cachedFile);
-          console.log(cachedFile);
-        }
-      });
-    });
-
-  } catch (e) {
-    console.error("String received was: " + string);
-    if (e.stack) {
-      console.error(e.stack);
+    if (argv.output) {
+      output = path.resolve(process.cwd(), argv.output);
+      if (fs.existsSync(output)) {
+        fs.unlinkSync(output);
+      }
+      fs.linkSync(apkLoc, output);
     }
+  } else {
+    console.error(err);
   }
+
 });
