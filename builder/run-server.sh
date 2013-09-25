63061064667e92f309dc30bae114c3b01a8e0c54
jhugman <jhugman@jhugman-10147.local>
2013-09-25 05:37:48 +0100

    Tidy the run script.
    It should be split into an update-server.

--- builder/run-server.sh
+++ builder/run-server.sh
@@ -1,12 +1,28 @@
 #!/bin/sh
-# 17:37:17 - bwalker: curl http://107.22.148.17:808080/
-npm install
+# 17:37:17 - bwalker: curl http://107.22.148.17:8080/
 
 tools=/tmp/tools
 cache=/tmp/apk-cache
 builddir=/tmp/android-projects
-android=$tools/android-sdk-linux/tools/android
-$android update project -p template
-$android update project -p ../library
-killall node
-(PATH=$tools/android-sdk-linux/tools:$tools/android-sdk-linux/platform-tools:$PATH ; /usr/bin/node lib/server.js -p 8080 -c $cache -d $builddir --force &)
+
+ANDROID_HOME=$tools/android-sdk-linux
+
+node=/usr/bin/node
+
+android=$ANDROID_HOME/tools/android
+
+# update npm.
+npm install
+
+# Tell the projects where the android-sdk is.
+
+# Get the latest android-sdk installed,
+target=$($android list target | grep -o android-[0-9]* | tail -n 1)
+$android update project -s --path ../library --target $target
+$android update project --path template
+
+
+# Restart the server.
+killall node 2>/dev/null 1>&2
+PATH=$ANDROID_HOME/tools:$ANDROID_HOME/platform-tools:$PATH
+$node lib/server.js -p 8080 -c $cache -d $builddir --force &
