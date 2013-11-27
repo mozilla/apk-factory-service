e98ba6ede83ac486122fb6bf6e81698852426d70
Austin King <shout@ozten.com>
2013-11-27 10:08:19 -0800

    Add metrics hooks. Add too busy middleware

--- bin/server.js
+++ bin/server.js
@@ -8,15 +8,19 @@ var path = require("path");
 var url = require('url');
 
 var express = require('express');
+var toobusy = require('../lib/busy_middleware.js');
 
 var _ = require("underscore");
 var ApkGenerator = require("../lib/apk-generator").ApkGenerator;
 var buildQueue = require("../lib/build_queue");
 var log = require('../lib/logging').logger;
+var metrics = require('../lib/metrics');
 
 require('../lib/config')(function (config) {
   var app = express();
+  app.use(toobusy);
   var appGenerator = function (request, response) {
+    var start = new Date();
 
     var generator = new ApkGenerator(config.buildDir,
                                      config.cacheDir,
@@ -25,22 +29,27 @@ require('../lib/config')(function (config) {
     var manifestUrl = request.query.manifestUrl;
     var appType = request.query.appType || "hosted";
 
-    if (!manifestUrl) {    
+    if (!manifestUrl) {
+      metrics.badManifestUrl();
       response.send("A manifestUrl param is needed", 400);
       return;
     }
+
+    metrics.generateApkRequest(manifestUrl);
 
     buildQueue(manifestUrl, function(finishedCb) {
       generator.generate(manifestUrl, null, appType, function (err, apkLoc) {
         if (err) {
   	  response.type("text/plain");
   	  response.send(err.toString(), 400);
+          metrics.generationApkFailed();
           finishedCb();
   	  return;
         }
         response.status(200);
         response.type("application/vnd.android.package-archive");
         response.sendfile(apkLoc);
+        metrics.generationApkFinished(new Date() - start);
         finishedCb();
       });
     });
@@ -57,6 +66,7 @@ require('../lib/config')(function (config) {
   app.get("/index.html", indexFile);
 
   app.listen(config.server_port, config.bind_address, function() {
+    metrics.serverStarted();
     log.info("running on " + config.bind_address + ":" + config.server_port);
   });
 });
