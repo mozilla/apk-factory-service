ea923453d37b7bea4cb2974cdae8c3c3b9e4e13a
Austin King <shout@ozten.com>
2013-12-04 16:14:06 -0800

    Move cli.js over to refactored code

--- bin/cli.js
+++ bin/cli.js
@@ -6,80 +6,88 @@
 
 "use strict";
 var optimist = require("optimist"),
-    path = require("path"),
-    fs = require("fs");
+path = require("path"),
+fs = require("fs");
+
+var ApkGenerator = require("../lib/apk_generator").ApkGenerator;
+var frontController = require('../lib/front_controller');
 
 var argv = optimist
-    .usage('Usage: $0 {OPTIONS}')
-    .wrap(80)
-    .option('manifest', {
-        alias: "m",
-        desc: "The URL or path to the manifest"
-    })
-    .option('overideManifest', {
-        desc: "The URL or path to the manifest"
-    })
-    .option('buildDir', {
-        alias: "d",
-        desc: "Use this directory as the temporary project directory",
-        default: path.resolve(process.env.TMPDIR || process.cwd(), "app")
-    })
-    .option('cacheDir', {
-        alias: "c",
-        desc: "Use this directory as the directory to cache keys and apks"
-    })
-    .option('force', {
-        alias: "f",
-        desc: "Force the projects to be built every time, i.e. don't rely on cached copies",
-        default: false,
-        boolean: true
-    })
-    .option("output", {
-        alias: "o",
-        desc: "The output APK filename"
-    })
-    .option("debug", {
-        desc: "Do not delete the project build directory",
-        default: false,
-        boolean: true
-    })
-    .option('help', {
-        alias: "?",
-        desc: "Display this message",
-        boolean: true
-    })
-    .check(function (argv) {
-        if (argv.help) {
-            throw "";
-        }
-
-        if (!argv.manifest) {
-          throw "Must specify a manifest location";
-        }
-
-
-        argv.buildDir = path.resolve(process.cwd(), argv.buildDir);
-
-    })
-    .argv;
-
-var ApkGenerator = require("../lib/apk_generator").ApkGenerator,
-    generator = new ApkGenerator(argv.buildDir, argv.cacheDir, argv.force, argv.debug);
-
-
-generator.generate(argv.manifest, argv.overideManifest, argv.type, function (err, apkLoc) {
-  var output;
-  if (!err) {
-
-    if (argv.output) {
-      output = path.resolve(process.cwd(), argv.output);
-      if (fs.existsSync(output)) {
-        fs.unlinkSync(output);
-      }
-      fs.linkSync(apkLoc, output);
+  .usage('Usage: $0 {OPTIONS}')
+  .wrap(80)
+  .option('manifest', {
+    alias: "m",
+    desc: "The URL or path to the manifest"
+  })
+  .option('overideManifest', {
+    desc: "The URL or path to the manifest"
+  })
+  .option('buildDir', {
+    alias: "d",
+    desc: "Use this directory as the temporary project directory",
+  default: path.resolve(process.env.TMPDIR || process.cwd(), "app")
+  })
+  .option('cacheDir', {
+    alias: "c",
+    desc: "Use this directory as the directory to cache keys and apks"
+  })
+  .option('force', {
+    alias: "f",
+    desc: "Force the projects to be built every time, i.e. don't rely on cached copies",
+  default: false,
+    boolean: true
+  })
+  .option("output", {
+    alias: "o",
+    desc: "The output APK filename"
+  })
+  .option("debug", {
+    desc: "Do not delete the project build directory",
+  default: false,
+    boolean: true
+  })
+  .option('help', {
+    alias: "?",
+    desc: "Display this message",
+    boolean: true
+  })
+  .check(function (argv) {
+    if (argv.help) {
+      throw "";
     }
-  } else {
-    console.error(err);
-  }
 
+    if (!argv.manifest) {
+      throw "Must specify a manifest location";
+    }
+
+
+    argv.buildDir = path.resolve(process.cwd(), argv.buildDir);
+
+  })
+  .argv;
+
+
+
+var withConfig = require('../lib/config');
+
+withConfig(function(config) {
+
+  var manifestUrl = argv.overideManifest || argv.manifest;
+  frontController(manifestUrl, argv.type, config, function(err, apkLoc) {
+    var output;
+    if (!err) {
+
+      if (argv.output) {
+	output = path.resolve(process.cwd(), argv.output);
+	if (fs.existsSync(output)) {
+          fs.unlinkSync(output);
+	}
+	fs.linkSync(apkLoc, output);
+      } else {
+	console.log('Build ', apkLoc);
+      }
+    } else {
+      console.error(err);
+    }
+  });
 });
