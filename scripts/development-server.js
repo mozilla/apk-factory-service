8322a6f9c704ce96ec6a2bbbf242a8f533bf3816
Austin King <shout@ozten.com>
2014-01-22 10:03:24 -0800

    Adding new development-server.js to run deamons

---
+++ scripts/development-server.js
@@ -0,0 +1,45 @@
+var path = require('path');
+var spawn = require('child_process').spawn;
+
+var controller = path.join(__dirname, '..', 'bin', 'controller');
+var generator = path.join(__dirname, '..', 'bin', 'generator');
+
+var children = [];
+
+['JAVA_HOME', 'ANDROID_HOME'].forEach(function(v) {
+  if (!process.env[v]) {
+    console.log("WARNING: You should set the environment variable " + v);
+  }
+});
+
+if (!process.env['CONFIG_FILES']) {
+  var c = [
+    path.join(__dirname, '..', 'config', 'default.js'),
+    path.join(__dirname, '..', 'config', 'developer.js')
+  ];
+  process.env['CONFIG_FILES'] = c.join(',');
+  console.log(process.env['CONFIG_FILES']);
+}
+
+function wireUp(child, prefix) {
+  children.push(child);
+  child.stdout.on('data', function(data) {
+    console.log(prefix, data.toString('utf8'));
+  });
+
+  child.stderr.on('data', function(data) {
+    console.log('ERROR ' + prefix, data.toString('utf8'));
+  });
+
+  child.on('close', function(code) {
+    children.forEach(function(aChild) {
+      aChild.kill('SIGHUP');
+    });
+  });
+}
+
+var cChild = spawn('node', [controller]);
+wireUp(cChild, 'CONTROLLR:');
+
+var gChild = spawn('node', [generator]);
+wireUp(gChild, 'GENERATOR:');
